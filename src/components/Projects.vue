971270817b07d94f459122ff123ce484d022a764
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-02 16:12:37 +0200

    FEAT using axios and getting Json with results

---
+++ src/components/Projects.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+    <p v-for="project in projects" :key="project.id">{{ project.title }}</p>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+  export default {
+    data(){
+      return{
+        projects: []    //array che inizialmente e' vuoto ma dopo verra' popolato con la chiamata axios
+      }
+    },
+
+    methods:{
+      fetchProjects(){ //metodo che recupera i projects
+ 
+        axios.get('http://127.0.0.1:8000/api/projects') //chiamata axios alla route api
+        .then(res =>{
+          console.log(res)      //se va ci da console log res
+        })
+        .catch(err =>{
+          console.log(err)      //se errore ci da console log error
+        })
+
+      }
+    },
+    mounted(){
+      this.fetchProjects()    //on mounted si attiva il metodo fatto sopra
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
