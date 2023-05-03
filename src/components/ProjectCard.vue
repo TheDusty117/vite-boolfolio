2dea9a46a5ca96e314e1fe444e0b8b95615062cc
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-03 10:21:27 +0200

    FEAT printing techs and categories on card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,6 +3,11 @@
     <h3>{{ project.title }}</h3>
     <h4>{{ project.client }}</h4>
     <p>{{ project.description }}</p>
+    <p>{{ project.category ? project.category.name : '-' }}</p>
+    <ul class="technology-list" v-if="project.technologies && project.technologies.length > 0">
+      <li class="technology" v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</li>
+    </ul>
+    
 
   </div>
 </template>
