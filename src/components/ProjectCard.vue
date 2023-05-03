940da5dcd2cd9495bd6430a5b5cd71979faee226
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-03 16:33:08 +0200

    FEAT router link to project  (task for tomorrow)

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="card m-4">
-    <h3>{{ project.title }}</h3>
-    <h4>{{ project.client }}</h4>
-    <p>{{ project.description }}</p>
-    <p>{{ project.category ? project.category.name : '-' }}</p>
-    <ul class="d-flex justify-content-center gap-3 technology-list" v-if="project.technologies && project.technologies.length > 0">
-      <li class="technology" v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</li>
-    </ul>
+    <!-- rotta che registreremo per mostrare quando clicchiamo un project -->
+    <router-link :to="`/${project.slug}`"> 
+
+      <h3>{{ project.title }}</h3>
+      <h4>{{ project.client }}</h4>
+      <p>{{ project.description }}</p>
+      <p>{{ project.category ? project.category.name : '-' }}</p>
+      <ul class="d-flex justify-content-center gap-3 technology-list" v-if="project.technologies && project.technologies.length > 0">
+        <li class="technology" v-for="technology in project.technologies" :key="technology.id">{{ technology.name }}</li>
+      </ul>
+
+    </router-link>
     
 
   </div>
