0d60abdb2164fdc987aeece76f4f9ce0bf03d755
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-03 16:30:54 +0200

    FEAT showing Projects in Portfolio route

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default{
 
 <template>
   <header>
-    <div class="container">
+    <div class="container-header">
       <h2>Boolfolio</h2>  
       <NavBar></NavBar>
     </div>
@@ -24,21 +24,38 @@ export default{
 
 
   <main>
+    <div class="container-main">
+
+    </div>
     <router-view></router-view>
   </main>
 
 
 
   <footer>
-    <h2>
-      questo e' un footer
-    </h2>
+    <div class="container-footer">
+      <h2>
+        questo e' un footer
+      </h2>
+    </div>
   </footer>
 
 </template>
 
 <style lang="scss">
 @use './style/general.scss' as *;
+
+.container-header{
+  
+}
+
+.container-main{
+  
+}
+
+.container-footer{
+  
+}
 
 
 </style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -2,13 +2,17 @@
   <div>
     <div class="container">
       <h1>PORTFOLIO</h1>
+      <Projects/>
     </div>
   </div>
 </template>
 
 <script>
+import Projects from '../components/Projects.vue';
   export default {
-    
+    components:{
+      Projects
+    }
   }
 </script>
 
